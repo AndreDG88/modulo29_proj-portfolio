ecd7cc2efe4612f301e78b433e3e4cec32a9acff
AndreDG88 <andre.soares.designer@gmail.com>
2025-01-08 09:40:54 -0300

    correção 01

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,17 @@
 import { ReportHandler } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = async (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
-    })
+    // Usando import dinâmico com async/await
+    const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import(
+      'web-vitals'
+    )
+
+    getCLS(onPerfEntry)
+    getFID(onPerfEntry)
+    getFCP(onPerfEntry)
+    getLCP(onPerfEntry)
+    getTTFB(onPerfEntry)
   }
 }
 
